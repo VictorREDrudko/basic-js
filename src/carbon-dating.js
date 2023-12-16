0647211bdc58555c2ea7ef34bb2ce96bf6779dc3
VictorREDrudko <viktor_RED_rudko@mail.ru>
2023-12-16 23:27:29 +0300

    feat: solve tasks 1 2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+	if (typeof(sampleActivity) !== 'string' ) {
+		return false;
+	}
+
+	if (typeof(+sampleActivity) === 'number' && +sampleActivity > 0 && +sampleActivity < 15) {
+	const res = Math.log(MODERN_ACTIVITY / +sampleActivity);
+	const k = 0.693 / HALF_LIFE_PERIOD;
+	
+	const t = res / k;
+	return Math.ceil(t);
+	}
+
+	return false;
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arrayS1 = s1.split('');
+  let arrayS2 = s2.split('');
+  let count = 0;
+
+  arrayS1.forEach(el => {
+		if (arrayS2.indexOf(el) !== -1) {
+			count = count + 1;
+			delete arrayS2[arrayS2.indexOf(el)];
+			console.log(arrayS2);
+		}
+  })
+
+  return count;
 }
 
 module.exports = {
